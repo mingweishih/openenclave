abd8635c2f5aa60d2213b715af438f6b923a2072
Ming-Wei Shih <mishih@microsoft.com>
2021-02-25 22:36:21 +0000

    test
    Signed-off-by: Ming-Wei Shih <mishih@microsoft.com>

---
+++ tests/scmml/enc/enc.c
@@ -0,0 +1,53 @@
+// Copyright (c) Open Enclave SDK contributors.
+// Licensed under the MIT License.
+
+#include <openenclave/enclave.h>
+#include <stdio.h>
+
+__attribute__((weak)) int foo(int a);
+__attribute__((weak)) int add(int a, int b);
+__attribute__((weak)) int sub(int a, int b);
+
+void enc_call_foo()
+{
+    if (foo)
+    {
+        printf("foo function defined in secondary module\n");
+        int value = foo(8);
+        printf("foo(8) = %d\n", value);
+    }
+    else
+    {
+        printf("foo not defined\n");
+    }
+
+    if (add)
+    {
+        printf("add function defined in secondary module\n");
+        int value = add(8, 7);
+        printf("add(8, 7) = %d (add adds 500 to result)\n", value);
+    }
+    else
+    {
+        printf("add not defined\n");
+    }
+
+    if (sub)
+    {
+        printf("sub function defined in secondary module\n");
+        int value = sub(8, 7);
+        printf("sub(8, 7) = %d\n", value);
+    }
+    else
+    {
+        printf("sub not defined\n");
+    }
+}
+
+OE_SET_ENCLAVE_SGX(
+    1,        /* ProductID */
+    1,        /* SecurityVersion */
+    true,     /* Debug */
+    3 * 1024, /* NumHeapPages */
+    64,       /* NumStackPages */
+    2);       /* NumTCS */
---
+++ tests/scmml/sm/sm.c
@@ -0,0 +1,18 @@
+// Copyright (c) Open Enclave SDK contributors.
+// Licensed under the MIT License.
+
+int foo(int a)
+{
+    return a * a;
+}
+
+int k = 500;
+
+int add(int a, int b)
+{
+    return a + b + k;
+}
+
+void _start()
+{
+}
